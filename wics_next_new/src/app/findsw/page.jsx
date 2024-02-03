0b2a7788235f606f08fd89cc0b36d212770b0176
hima tummalapalli <himatummalapalli@wireless-10-146-13-24.public.utexas.edu>
2024-02-03 16:07:26 -0600

    Finished show Sure Walk Number functionality

--- wics_next_new/src/app/findsw/page.jsx
+++ wics_next_new/src/app/findsw/page.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
-import Card from '../components/Card'
 
 
 export default function FindSW() {
     return(
         <>
-            <Card/>
         </>
     );
 }
--- wics_next_new/src/app/getnum/page.jsx
+++ wics_next_new/src/app/getnum/page.jsx
@@ -1,9 +1,60 @@
+'use client'
 import React from 'react'
+import "../globals.css"
+import { useState } from "react"
+import Navbar from '../components/Navbar'
 
 export default function GetNumber() {
-    return(
+    const [info, setInfo] = useState("")
+    const [showVanNum, setShowVanNum] = useState(false)
+    const [signedUp, setSignedUp] = useState(false)
+    const [notSignedUp, setNotSignedUp] = useState(true)
+    const [num, setNum] = useState(-1)
+
+    const handleChange = (event) => {
+        setShowVanNum(false)
+        setInfo(event.target.value)
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        setShowVanNum(true)
+        if (info == "bb5555") {
+            setSignedUp(true)
+            setNotSignedUp(false)
+            setNum(353203)
+        } else {
+            setSignedUp(false)
+            setNotSignedUp(true)
+            setNum(-1)
+        }
+        
+    }
+
+    return (
         <>
-            <p> Hello </p>
+            <Navbar></Navbar>
+            <div class="card" style={{height: "350px", width: "300px", backgroundColor: "orange", borderRadius: "5px"}}>
+            <div class="card-body" style={{paddingTop: "20px", paddingLeft: "10px"}}>
+                <h2 class="card-title"> Get Sure Walk Number </h2>
+                <br></br>
+                <form onSubmit={handleSubmit}>
+                <label style={{margin: "auto"}}>Enter UT EID
+                <br></br>
+                    <input 
+                        type="text" 
+                        value={info}
+                        onChange= {handleChange}
+                    />
+                </label>
+                <br></br>
+                <input type="submit"></input>
+                </form>
+            </div>
+        </div>
+        {showVanNum && signedUp && <p>Your Sure Walk Number is {num}</p>}
+        {showVanNum && notSignedUp && <p>You have not signed up for a sure walk</p>}
+
         </>
-    );
+    )
 }
