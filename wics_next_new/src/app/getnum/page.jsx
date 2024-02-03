93be127394b25dadaffbdb84c7d812616c2fd41f
Tejaswi Thapa <tejaswithapa@wireless-10-146-104-18.public.utexas.edu>
2024-02-03 17:03:29 -0600

    Finalized styling for get num

--- wics_next_new/src/app/getnum/page.jsx
+++ wics_next_new/src/app/getnum/page.jsx
@@ -34,32 +34,33 @@ export default function GetNumber() {
     return (
         <>
           <Navbar></Navbar>
-            <div style={{height: "700px", display: "flex", alignItems: "center", justifyContent: "center"}}>
-                <div className="card" style={{
-                    height: "300px", width: "325px", backgroundColor: "#f69521",
-                    borderRadius: "5px", display: "flex", alignItems: "center", justifyContent: "center", marginTop: "auto",
-                    marginBottom: "auto"}}>
-                    <div className="card-body" style={{paddingTop: "20px", paddingLeft: "10px"}}>
-                        <h2 className="card-title"> Get Sure Walk Number </h2>
-                        <br></br>
-                        <form onSubmit={handleSubmit}>
-                        <label style={{margin: "auto"}}>Enter UT EID
-                        <br></br>
-                            <input 
-                                type="text" 
-                                value={info}
-                                onChange= {handleChange}
-                            />
-                        </label>
-                        <br></br>
-                        <input type="submit"></input>
-                        </form>
-                    </div>
-                </div>
-                {showVanNum && signedUp && <p style={{ paddingTop: "20px" }}>Your Sure Walk Number is {num}</p>}
-                {showVanNum && notSignedUp && <p>You have not signed up for a sure walk</p>}
+          <div style={{display: "block", textAlign: "center", padding: "100px 400px 125px 400px"}}>
+          <div className="card" style={{
+             height: "300px", width: "325px", backgroundColor: "#f69521",
+             borderRadius: "5px", display: "flex", alignItems: "center", justifyContent: "center"}}>
+            <div className="card-body" style={{paddingLeft: "10px"}}>
+                <h2 className="card-title"> Get Sure Walk Number </h2>
+                   <br></br>
+                   <br></br>
+                <form onSubmit={handleSubmit}>
+                      <label style={{ margin: "auto" }}>Enter UT EID
+                         <br></br>
+                <br></br>
+                    <input 
+                        type="text" 
+                        value={info}
+                        onChange= {handleChange}
+                    />
+                </label>
+                      <br></br>
+                      <br></br>
+                <input type="submit"></input>
+                </form>
             </div>
-          
+        </div>
+             {showVanNum && signedUp && <p style={{ paddingTop: "20px" }}>Your Sure Walk Number is {num}</p>}
+        {showVanNum && notSignedUp && <p>You have not signed up for a sure walk</p>}
+          </div>
           </>
     )
 }
