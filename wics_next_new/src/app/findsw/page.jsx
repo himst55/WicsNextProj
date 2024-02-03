8bb66f4b7dcd3e6d1e7efb5a23806a55e1799802
hima tummalapalli <himatummalapalli@wireless-10-146-13-24.public.utexas.edu>
2024-02-03 16:30:35 -0600

    Added Sure Walk Location Information

--- wics_next_new/src/app/findsw/page.jsx
+++ wics_next_new/src/app/findsw/page.jsx
@@ -1,9 +1,52 @@
+'use client'
 import React from 'react'
-
+import "../globals.css"
+import { useState } from "react"
+import Navbar from '../components/Navbar'
+import Link from 'next/link'
 
 export default function FindSW() {
-    return(
+    const [info, setInfo] = useState("")
+    const [showVanLoc, setShowVanLoc] = useState(false)
+
+    const handleChange = (event) => {
+        setShowVanLoc(false)
+        setInfo(event.target.value)
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        if (info == "353203") {
+            setShowVanLoc(true)
+            alert("You Entered a Valid Sure Walk Number. Please Click the \"See Location \" button to find your Sure Walk.")
+        } else {
+            alert("Not a valid Sure Walk Number")
+        }
+        
+    }
+
+    return (
         <>
+            <Navbar></Navbar>
+            <div class="card" style={{height: "350px", width: "300px", backgroundColor: "orange", borderRadius: "5px"}}>
+            <div class="card-body" style={{paddingTop: "20px", paddingLeft: "10px"}}>
+                <h2 class="card-title"> Get Sure Walk Number </h2>
+                <br></br>
+                <form onSubmit={handleSubmit}>
+                <label style={{margin: "auto"}}>Enter Sure Walk Number
+                <br></br>
+                    <input 
+                        type="text" 
+                        value={info}
+                        onChange= {handleChange}
+                    />
+                </label>
+                <br></br>
+                <input type="submit"></input>
+                </form>
+            </div>
+            {showVanLoc && <button><Link href="/showSW/info">See Location</Link></button>}
+        </div>
         </>
     );
 }
--- wics_next_new/src/app/showSW/[vanNum]/page.jsx
+++ wics_next_new/src/app/showSW/[vanNum]/page.jsx
@@ -1,5 +1,12 @@
 import React from 'react'
 
 export default function showSW() {
-    
+    return(
+        <>
+            <h2> Sure Walk Information </h2>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3445.3816368885214!2d-97.74142618827666!3d30.28319530711323!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8644b59c34f0d4ff%3A0x26c69e778a494e85!2sUniversity%20Teaching%20Center%20(UTC)!5e0!3m2!1sen!2sus!4v1706999203772!5m2!1sen!2sus" width="600" height="450" style={{border: "0;"}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            <p> Your Sure Walk will arrive in 20 minutes. </p>
+        </>
+    )
+
 }
