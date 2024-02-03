06f48e87c43433485e6083657d0abf9b08caa0e0
Tejaswi Thapa <tejaswithapa@wireless-10-146-104-18.public.utexas.edu>
2024-02-03 16:11:48 -0600

    HomePage

--- wics_next_new/src/app/components/Navbar.js
+++ wics_next_new/src/app/components/Navbar.js
@@ -5,10 +5,10 @@ import Image from 'next/Image'
 export default function Navbar() {
     return (
         <div className="navbar" style={{height: "80px", backgroundColor: "#F69521", alignContent: "center"}}>
-            <Image src="/sure-walk.png" alt="Sure Walk Logo" width={75} height={75} style={{width: "150px", height: "80px"}}></Image>
-            <Link href="/"> Home </Link>
-            <Link href="/findsw"> Find Sure Walk </Link>
-            <Link href="/getnum"> Get Sure Walk Number </Link>
+            <Image src="/sure-walk.png" alt="Sure Walk Logo" width={75} height={75} style={{width: "175px", height: "80px"}}></Image>
+          <Link href="/" style={{padding: "50px"}}> Home </Link>
+            <Link href="/findsw" style={{padding: "50px" }}> Find Sure Walk </Link>
+            <Link href="/getnum" style={{padding: "50px" }}> Get Sure Walk Number </Link>
         </div>
     );
 }
